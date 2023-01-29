7028a48d24dcfa2281d62016874df73423f14fb7
newlive713310 <newlive713310@gmail.com>
2023-01-29 20:36:18 +0600

    Fix

---
+++ ChatApi/Program.cs
@@ -0,0 +1,14 @@
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
